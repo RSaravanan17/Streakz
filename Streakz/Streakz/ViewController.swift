f94fbfb1d67708c5504a5631e924b47308d699b8
Michael Munoz <michael.munoz1101@utexas.edu>
2021-03-17 12:20:57 -0500

    Add starter code

---
+++ Streakz/Streakz/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Streakz
+//
+//  Created by Michael on 3/17/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
