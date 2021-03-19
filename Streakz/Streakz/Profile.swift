fd1ef5fc1993dbdc856aa7ac627594e18bec38f1
jhcauvin <johncauvin@gmail.com>
2021-03-18 22:03:47 -0500

    Added two model files with commented ideas

---
+++ Streakz/Streakz/Profile.swift
@@ -0,0 +1,18 @@
+//
+//  Profile.swift
+//  Streakz
+//
+//  Created by John Cauvin on 3/18/21.
+//
+
+import Foundation
+
+class Profile {
+    /*
+     First name
+     last name
+     Profile picture
+     Streaks subscribed to (list of streaks)
+     Streak counters (probably map of streak -> counter). Can be used to fetch personal best etc.
+     */
+}
---
+++ Streakz/Streakz/Streak.swift
@@ -0,0 +1,19 @@
+//
+//  Streak.swift
+//  Streakz
+//
+//  Created by John Cauvin on 3/18/21.
+//
+
+import Foundation
+
+class Streak {
+    /*
+     frequency
+     name
+     description
+     privacy setting (public or private or friend's only)
+     owner
+     subscribers/members
+     */
+}
