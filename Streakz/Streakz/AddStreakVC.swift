faf1845e78471609ffa5dcc97d36e4a4ed238e11
Michael Munoz <michael.munoz1101@utexas.edu>
2021-03-24 13:51:37 -0500

    Finish styling Add Streakz Screen

--- Streakz/Streakz/AddStreakVC.swift
+++ Streakz/Streakz/AddStreakVC.swift
@@ -7,13 +7,51 @@
 
 import UIKit
 
-class AddStreakVC: UIViewController, UITextViewDelegate {
+class AddStreakVC: UIViewController, UITextViewDelegate, UIPickerViewDataSource, UIPickerViewDelegate {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 3
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        if component == 0 {
+            return 10
+        } else {
+            return 100
+        }
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        if component == 0 {
+            return "Only Me"
+        } else if component == 1 {
+            return "Friends"
+        } else {
+            return "Anyone"
+        }
+    }
+    
     
     // outlets
     @IBOutlet weak var descTextView: UITextView!
     @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var addStreakButton: UIButton!
+    @IBOutlet weak var viewSunday: UIView!
+    @IBOutlet weak var viewMonday: UIView!
+    @IBOutlet weak var viewTuesday: UIView!
+    @IBOutlet weak var viewWednesday: UIView!
+    @IBOutlet weak var viewThursday: UIView!
+    @IBOutlet weak var viewFriday: UIView!
+    @IBOutlet weak var viewSaturday: UIView!
     
+    @IBOutlet weak var reminderTimePicker: UIDatePicker!
+    @IBOutlet weak var visibilityPicker: UIPickerView!
+    
+    var daysOfWeekViews: [UIView?] = []
+    var daysOfWeekButtons: [UIButton] = []
+    let daysOfWeekTitles = ["Su", "M", "Tu", "W", "Th", "F", "Sa"]
+    var daysOfWeekSelected = [false, false, false, false, false, false, false]
+    
+    var sundayPressed: Bool = false
     let descPlaceholder = "Do push ups three times a week for the gains"
     let textFieldGray = UIColor.gray.withAlphaComponent(0.5)
     
@@ -34,6 +72,40 @@ class AddStreakVC: UIViewController, UITextViewDelegate {
         addStreakButton.backgroundColor = UIColor.init(named: "Streakz_DarkRed")
         addStreakButton.layer.cornerRadius = 25
         addStreakButton.tintColor = UIColor.black
+        
+        daysOfWeekViews = [
+            viewSunday,
+            viewMonday,
+            viewTuesday,
+            viewWednesday,
+            viewThursday,
+            viewFriday,
+            viewSaturday
+        ]
+        
+        // Testing for button
+        for (i, view) in daysOfWeekViews.enumerated() {
+            let button = UIButton(type: .custom)
+            button.frame = CGRect(x: 0, y: 0, width: 32, height: 32)
+            button.backgroundColor = UIColor.gray
+            button.layer.cornerRadius = 0.5 * button.bounds.size.width
+            button.clipsToBounds = true
+            button.setTitle(daysOfWeekTitles[i], for: .normal)
+            button.setTitleColor(UIColor.black, for: .normal)
+            button.addTarget(self, action: #selector(dayPressed), for: .touchUpInside)
+            daysOfWeekButtons.append(button)
+            view?.addSubview(button)
+        }
+}
+    
+    @objc func dayPressed(sender: UIButton!) {
+        let index = daysOfWeekTitles.firstIndex(of: sender.title(for: .normal)!)!
+        daysOfWeekSelected[index] = !daysOfWeekSelected[index]
+        if daysOfWeekSelected[index] {
+            daysOfWeekButtons[index].backgroundColor = UIColor.init(named: "Streakz_DarkRed")
+        } else {
+            daysOfWeekButtons[index].backgroundColor = UIColor.gray
+        }
     }
     
 
