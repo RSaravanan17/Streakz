2340872dad362fc6c1378e91c2ba27f62b3ae914
jhcauvin <johncauvin@gmail.com>
2021-03-28 14:43:18 -0500

    Streak posts added to backend now on streak completion

--- Streakz/Streakz/StreakPost.swift
+++ Streakz/Streakz/StreakPost.swift
@@ -20,4 +20,20 @@ class StreakPost : Codable {
      date of post (needed for friend's feed)
      privacy: inherit privacy setting from the streaksubscription object. private/friend's only, possibly public for later if we want to display public posts on view streak screen
      */
+    
+    var streak: StreakSubscription
+    var postText: String
+    var image: String
+    var datePosted: Date
+    var achievedStreak: Int
+    
+    init(for streak: StreakSubscription, postText: String, image: String) {
+        self.streak = streak
+        self.postText = postText
+        self.image = image
+        // capture current date for when post was made
+        datePosted = Date()
+        // achieved streak is the streak number accomplished when the post is created
+        achievedStreak = streak.streakNumber
+    }
 }
