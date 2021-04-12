99818f042c21eadca3e42834c558394f4a1d5ee7
zjbonar <zjbonar@gmail.com>
2021-04-12 17:39:42 -0500

    clean up view streak post screen

--- Streakz/Streakz/StreakPostScreenViewController.swift
+++ Streakz/Streakz/StreakPostScreenViewController.swift
@@ -11,7 +11,7 @@ class StreakPostScreenViewController: UIViewController {
 
     @IBOutlet weak var streakTitle: UILabel!
     @IBOutlet weak var postNumber: UILabel!
-    @IBOutlet weak var streakDescription: UILabel!
+    @IBOutlet weak var streakPostDescription: UILabel!
     @IBOutlet weak var postImage: UIImageView!
     
     var streakPostDelegate: ProfileVC! = nil
@@ -22,12 +22,12 @@ class StreakPostScreenViewController: UIViewController {
         // allows for unlimited number of lines for labels
         self.streakTitle.numberOfLines = 0
         self.postNumber.numberOfLines = 0
-        self.streakDescription.numberOfLines = 0
+        self.streakPostDescription.numberOfLines = 0
         
         // assign the labels to the appropriate fields from the current streak post
         self.streakTitle.text = self.streakPostDelegate.currentStreakPost!.streak.name
         self.postNumber.text = "Streak: \(self.streakPostDelegate.currentStreakPost!.achievedStreak)"
-        self.streakDescription.text = self.streakPostDelegate.currentStreakPost!.postText
+        self.streakPostDescription.text = self.streakPostDelegate.currentStreakPost!.postText
         self.postImage.image = UIImage(named: "StreakzLogo")
         if self.streakPostDelegate.currentStreakPost!.image == "" {
             self.postImage.image = UIImage(named: "StreakzLogo")
