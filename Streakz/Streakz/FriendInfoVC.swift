2b5c88fed86184c8dd194ac053da2bd94dead525
Michael Munoz <michael.munoz1101@utexas.edu>
2021-04-22 21:38:00 -0500

    Add tableview to friends list

--- Streakz/Streakz/FriendInfoVC.swift
+++ Streakz/Streakz/FriendInfoVC.swift
@@ -8,18 +8,50 @@
 import UIKit
 
 class FriendInfoVC: UIViewController {
+    private lazy var friendsListVC: FriendsListVC = {
+        // Load Storyboard
+        let storyboard = UIStoryboard(name: "Main", bundle: Bundle.main)
+
+        // Instantiate View Controller
+        var viewController = storyboard.instantiateViewController(withIdentifier: "FriendsListVC") as! FriendsListVC
+
+        // Add View Controller as Child View Controller
+        self.add(asChildViewController: viewController)
+
+        return viewController
+    }()
+
+    private lazy var friendRequestsVC: FriendRequestsVC = {
+        // Load Storyboard
+        let storyboard = UIStoryboard(name: "Main", bundle: Bundle.main)
+
+        // Instantiate View Controller
+        var viewController = storyboard.instantiateViewController(withIdentifier: "FriendRequestsVC") as! FriendRequestsVC
+
+        // Add View Controller as Child View Controller
+        self.add(asChildViewController: viewController)
+
+        return viewController
+    }()
 
     @IBOutlet weak var segmentedControl: UISegmentedControl!
+    @IBOutlet weak var containerView: UIView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
         
-        self.setUpSegmentedControl()
+        self.setupView()
     }
     
-    func setUpSegmentedControl() {
+    func setupView() {
+        self.setupSegmentedControl()
+        
+        self.updateView()
+    }
+    
+    func setupSegmentedControl() {
         // Configure Segmented Control
         self.segmentedControl.addTarget(self, action: #selector(selectionDidChange(_:)), for: .valueChanged)
 
@@ -28,7 +60,33 @@ class FriendInfoVC: UIViewController {
     }
     
     @objc func selectionDidChange(_ sender: UISegmentedControl) {
-//        updateView()
+        self.updateView()
+    }
+    
+    private func updateView() {
+        if self.segmentedControl.selectedSegmentIndex == 0 {
+            self.remove(asChildViewController: friendRequestsVC)
+            self.add(asChildViewController: friendsListVC)
+        } else {
+            self.remove(asChildViewController: friendsListVC)
+            self.add(asChildViewController: friendRequestsVC)
+        }
+    }
+    
+    func add(asChildViewController viewController: UIViewController) {
+        // Add Child View Controller
+        self.containerView.addSubview(viewController.view)
+    }
+    
+    private func remove(asChildViewController viewController: UIViewController) {
+        // Notify Child View Controller
+        viewController.willMove(toParent: nil)
+
+        // Remove Child View From Superview
+        viewController.view.removeFromSuperview()
+
+        // Notify Child View Controller
+        viewController.removeFromParent()
     }
     
 
--- Streakz/Streakz/FriendsListVC.swift
+++ Streakz/Streakz/FriendsListVC.swift
@@ -7,23 +7,84 @@
 
 import UIKit
 
-class FriendsListVC: UIViewController {
+class FriendsListVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var searchBar: UISearchBar!
+    
+    let friendCellIdentifier = "FriendCellIdentifier"
+    
+    var friends: [Profile] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        self.loadFriends()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewWillAppear(_ animated: Bool) {
+        self.loadFriends()
     }
-    */
+    
+    func loadFriends() {
+        for friend in cur_user_profile?.friends ?? [] {
+            db_firestore.collection(friend.profileType).document(friend.email).getDocument {
+                (document, error) in
+                let result = Result {
+                    try document?.data(as: Profile.self)
+                }
+                switch result {
+                case .success(let fetchedProfile):
+                    self.friends.append(fetchedProfile!)
+                case .failure(let error):
+                    print("Error fetching a profile on discover screen: \(error)")
+                }
+            }
+            self.tableView.reloadData()
+        }
+        self.tableView.reloadData()
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return friends.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: self.friendCellIdentifier, for: indexPath as IndexPath) as! FriendTableViewCell
+        let row = indexPath.row
+        let friend = self.friends[row]
+        print(friend.firstName)
+        print(friend.lastName)
+        cell.styleViewWith(friend)
+        return cell
+    }
+}
 
+class FriendTableViewCell: UITableViewCell {
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var streakLabel: UILabel!
+    
+    func styleViewWith(_ profile: Profile) {
+        // Style image view
+        self.profileImage.layer.borderWidth = 1.0
+        self.profileImage.layer.masksToBounds = false
+        self.profileImage.layer.borderColor = UIColor.white.cgColor
+        self.profileImage.layer.cornerRadius = self.profileImage.frame.size.width / 2
+        self.profileImage.clipsToBounds = true
+        
+        // Set fields
+        let imageURL: String = profile.profilePicture
+        if imageURL == "" {
+            self.profileImage.image = UIImage(named: "ProfileImageBlank")
+        } else {
+            self.profileImage.load(url: URL(string: imageURL)!)
+        }
+        self.nameLabel.text = "\(profile.firstName) \(profile.lastName)"
+        
+        let activeStreakCount = profile.subscribedStreaks
+        self.streakLabel.textColor = UIColor(named: "Streakz_Grey")
+        self.streakLabel.text = "\(activeStreakCount) Active Streakz"
+    }
 }
